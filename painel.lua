81f8878e28bae77cda87bcd3b26137157351292b
lynx782 <trulyn999@yahoo.com>
2025-11-03 17:19:30 -0300

    Removido Kaua_452 via bot

--- painel.lua
+++ painel.lua
@@ -25,7 +25,6 @@ local Autorizados = {
     ["marcelobaida9f"] = "Usuario-Admin",
     ["robaromeubranrot"] = "Usuario-Admin",
     ["miuuq_333"] = "Usuario-Admin",
-    ["Kaua_452"] = "Staff",
 }
 
 --// Jogadores ativos
