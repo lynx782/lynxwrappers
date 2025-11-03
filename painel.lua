6e5cc83f6a6c461f595ea07f1ac3a5249f92a330
lynx782 <trulyn999@yahoo.com>
2025-11-03 20:31:27 -0300

    Removido foortaq via bot

--- painel.lua
+++ painel.lua
@@ -26,7 +26,6 @@ local Autorizados = {
     ["robaromeubranrot"] = "Usuario-Admin",
     ["miuuq_333"] = "Usuario-Admin",
     ["BD_GOKENNY"] = "Usuario-Admin",
-    ["foortaq"] = "Usuario-Admin",
 }
 
 --// Jogadores ativos
